3cdfe8045d5666b2032a5f43b5ccb97a9ce044ee
khoirulmuasin <gabutcrypto1@gmail.com>
2024-11-09 22:12:58 +0700

    menambahkan struktur folder

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman about</title>
+    <link rel="stylesheet" href="css.style.css">
+</head>
+
+<body>
+    <a href="/">home</a>
+    <a href="/">about</a>
+    <h1>halaman about</h1>
+    <h1>halaman about foto</h1>
+    <h3>dear: {{ $nama }}</h3>
+    <img src="img/foto.png" alt="Tentang ku" width="200">
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=  , initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman home</title>
+    <link rel="stylesheet" href="css.style.css">
+</head>
+
+<body>
+    <a href="/">home</a>
+    <a href="/">about</a>
+    <h1>halaman home</h1>
+
+    <script src="js/script.js"></script>
+</body>
+
+</html>
